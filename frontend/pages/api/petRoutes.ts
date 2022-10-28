6d297e41709d9cc1f1d029db40230ac4596fea37
Erick <ericklima63@gmail.com>
2022-10-28 13:57:40 -0300

    Pet Routers Done

--- frontend/pages/api/petRoutes.ts
+++ frontend/pages/api/petRoutes.ts
@@ -22,16 +22,59 @@ interface petDataTypes {
     moreInfo: string
 }
 
-const config = (route: string, petInfo: petDataTypes) => {
+const config = (route: string, body?: petDataTypes, query?: string) => {
+
+    let methodUsedByRoute: string = "";
+    let headerUsedByRoute: {};
+
+    switch (route) {
+
+        case '/all' || '/my-pets' || '/pet':
+
+            methodUsedByRoute = "GET"
+
+            headerUsedByRoute = {
+                "Content-Type": "application/json"
+            }
+
+        case '/register':
+            methodUsedByRoute = "POST"
+
+            headerUsedByRoute = {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${userToken}`
+            }
+
+        case '/update-pet-status':
+            methodUsedByRoute = "PUT"
+
+            headerUsedByRoute = {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${userToken}`
+            }
+
+        case '/remove-pet':
+            methodUsedByRoute = "DELETE"
+
+            headerUsedByRoute = {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${userToken}`
+            }
+
+        default:
+            methodUsedByRoute = "GET" //fix
+
+            headerUsedByRoute = {
+                "Content-Type": "application/json"
+            }
+
+    }
 
     return {
-        method: "POST",
-        url: `${DB_URL}${route}`,
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${userToken}`
-        },
-        data: petInfo
+        method: methodUsedByRoute,
+        url: `${DB_URL}${route}${query && query}`,
+        headers: headerUsedByRoute,
+        data: body
     }
 
 }
@@ -47,16 +90,139 @@ export const createPetPost = async (info: petDataTypes) => {
 
             if (response.status === 201) {
 
-                
+                return {
+                    message: 'Success'
+                }
 
             }
 
         })
 
+
     }
     catch (error: any) {
 
         console.log(error)
+
+    }
+
+}
+
+export const getAllPetsByQuery = async (query?: { state: string, county?: string }) => {
+
+    let queryOnUrl: string = '';
+
+    // sets how will the query be on the url
+    if (query) {
+        if (query.state !== "" && query.county === "") {
+
+            queryOnUrl = `?state=${query.state}`
+
+        }
+        if (query.state !== "" && query.county !== "") {
+
+            queryOnUrl = `?state=${query.state}&county=${query.county}`
+
+        }
+
+    }
+
+    try {
+        const { data } = await axios(
+            config(`/all`, undefined, queryOnUrl && queryOnUrl)
+        )
+
+        return data;
+
+    }
+    catch (error: any) {
+
+        return {
+            message: error
+        }
+
+    }
+}
+
+// gets a pet info by getting its id and passing it through the url
+export const getPetInfo = async (query: string) => {
+
+    try {
+        const { data } = await axios(
+            config(`/pet`, undefined, `?id=${query}`)
+        )
+
+        return data;
+
+    }
+    catch (error: any) {
+
+        return {
+            message: error
+        }
+
+    }
+
+}
+
+// gets info of the pets registed by the user
+export const getPetsRegisteredByUser = async () => {
+
+    try {
+        const { data } = await axios(
+            config(`/my-pets`)
+        )
+
+        return data;
+
+    }
+    catch (error: any) {
+
+        return {
+            message: error
+        }
+
+    }
+
+}
+
+// sets pet status of wasFound previously from FALSE to TRUE
+export const updatePetStatus = async () => {
+
+    try {
+        const { data } = await axios(
+            config(`/update-pet-status`)
+        )
+
+        return data;
+
+    }
+    catch (error: any) {
+
+        return {
+            message: error
+        }
+
+    }
+
+}
+
+// removes pet info from Database
+export const removePetFromDB = async () => {
+
+    try {
+        const { data } = await axios(
+            config('/remove-pet')
+        )
+
+        return data;
+
+    }
+    catch (error: any) {
+
+        return {
+            message: error
+        }
 
     }
 
